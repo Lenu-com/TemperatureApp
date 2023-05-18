5579eda7f26433a5d2f705fdbeee0301fdea0ca4
Lenu_com <oretyan@icloud.com>
2023-05-19 07:19:01 +0900

    摂氏から華氏へ変換するロジックの実装 ボタンクリックリスナーの実装

--- app/src/main/java/com/websarva/wings/android/temperatureapp/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/temperatureapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.websarva.wings.android.temperatureapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvCelsius: TextView = findViewById(R.id.tvCelsius)
+        val tvFahrenheit: TextView = findViewById(R.id.tvFahrenheit)
+        val etCelsius: EditText? = findViewById(R.id.etCelsius)
+        val btConvert: Button = findViewById(R.id.btConvert)
+        var tvConvertFahrenheit: TextView = findViewById(R.id.tvConvertFahrenheit)
+
+        btConvert.setOnClickListener {
+            val celsius = createCelsiusBigDecimal(etCelsius)
+            val fahrenheitText = fahrenheitToString(convertCelsiusToFahrenheit(celsius))
+            tvConvertFahrenheit.text = fahrenheitText
+        }
+    }
+
+    private fun createCelsiusBigDecimal(etCelsius: EditText?): BigDecimal {
+        if (etCelsius == null) return BigDecimal("0")
+        return BigDecimal(etCelsius.text.toString())
+    }
+
+    private fun convertCelsiusToFahrenheit(celsiusBigDecimal: BigDecimal): BigDecimal {
+        return  celsiusBigDecimal * BigDecimal("1.8") + BigDecimal("32")
+    }
+
+    private fun fahrenheitToString(fahrenheit: BigDecimal): String {
+        return fahrenheit.toString()
     }
 }
